dda7d03017d4b5157ff9c61060c5449a13367cba
Lex010 <lex07672@gmail.com>
2023-04-17 15:06:29 +0300

    01

---
+++ changsTx03.js
@@ -0,0 +1,6 @@
+const mWork03 = document.getElementById('m_w03')
+const text03 = document.getElementById('video')
+mWork03.onclick = function() {
+    text03.style.animation = 'anim02 infinite 2s'
+
+}
