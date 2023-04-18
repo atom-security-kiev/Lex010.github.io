0b05b84508e1e959398a9d8d8aba22f28b2641a6
Lex010 <lex07672@gmail.com>
2023-04-18 10:15:04 +0300

    18.04

--- changeText/changsTx.js
+++ changeText/changsTx.js
@@ -1,8 +1,22 @@
 const mWork = document.getElementById('m_w')
 const text04 = document.getElementById('text04')
-mWork.onclick = function() {
+
+mWork.addEventListener('click',
+function() {
+    text04.classList.add('anim002')
+});
+
+text04.addEventListener("animationend", animEnd, false);
+function animEnd () {
+ text04.classList.remove('anim002');   
+}
+
+
+
+
+/*mWork.onclick = function() {
     text04.style.animation = 'anim02 infinite 2s'
 
-}
+}*/
 
 
